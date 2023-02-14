a3b993e85108d6cde69a3d5048360225d74fb0fb
Matheus Boechat <matheusfgb@gmail.com>
2023-02-14 13:33:51 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
+const circles = document.getElementById('circles');
+const balls = document.getElementsByClassName('ball');
+const rgbP = document.querySelector('p');
+const pAnswer = document.getElementById('answer');
+
+const generateRandomColor = () => {
+  const rRandom = Math.floor(Math.random() * 255);
+  const gRandom = Math.floor(Math.random() * 255);
+  const bRandom = Math.floor(Math.random() * 255);
+  return `rgb(${rRandom}, ${gRandom}, ${bRandom})`;
+};
+
+const getBgColorToP = (ballArray) => {
+  const randomBall = Math.floor(Math.random() * ballArray.length);
+  const rgbFromRandomBall = ballArray[randomBall].style.backgroundColor;
+  let excludesRgb = '';
+  for (let i = 3; i < rgbFromRandomBall.length; i+= 1) {
+    excludesRgb += rgbFromRandomBall[i];
+  }
+  return excludesRgb;
+};
+
 window.onload = () => {
-  const balls = document.getElementsByClassName('ball');
-  const generateRandomColor = () => {
-    const rRandom = Math.floor(Math.random() * (255 - 0) + 0);
-    const gRandom = Math.floor(Math.random() * (255 - 0) + 0);
-    const bRandom = Math.floor(Math.random() * (255 - 0) + 0);
-    return `rgb(${rRandom}, ${gRandom}, ${bRandom})`;
-  };
   for (let i = 0; i < balls.length; i += 1) {
     balls[i].style.backgroundColor = generateRandomColor();
   }
+  rgbP.innerText = getBgColorToP(balls);
 };
+
+circles.addEventListener('click', (event) => {
+  if (event.target.style.backgroundColor.includes(rgbP.innerText)) {
+    pAnswer.innerText = 'Acertou!';
+  } else {
+    pAnswer.innerText = 'Errou! Tente novamente!';
+  }
+});
