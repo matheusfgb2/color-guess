15c3a2a863ae6291ebd07cd6f52e6f5147fe8ae0
Matheus Boechat <matheusfgb@gmail.com>
2023-02-14 13:51:33 -0300

    ultimo requisito

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const circles = document.getElementById('circles');
 const balls = document.getElementsByClassName('ball');
 const rgbP = document.querySelector('p');
 const pAnswer = document.getElementById('answer');
+const pScore = document.getElementById('score');
 const resetBtn = document.getElementById('reset-game');
 
 const generateRandomColor = () => {
@@ -26,11 +27,22 @@ window.onload = () => {
     balls[i].style.backgroundColor = generateRandomColor();
   }
   rgbP.innerText = getBgColorToP(balls);
+  if (localStorage.getItem('placar')) {
+    pScore.innerText = `Placar: ${localStorage.getItem('placar')}`;
+  }
 };
+
+let score = 0;
+if (localStorage.getItem('placar')) {
+  score = +localStorage.getItem('placar');
+}
 
 circles.addEventListener('click', (event) => {
   if (event.target.style.backgroundColor.includes(rgbP.innerText)) {
     pAnswer.innerText = 'Acertou!';
+    score += 3;
+    pScore.innerText = `Placar: ${score}`;
+    localStorage.setItem('placar', score);
   } else {
     pAnswer.innerText = 'Errou! Tente novamente!';
   }
